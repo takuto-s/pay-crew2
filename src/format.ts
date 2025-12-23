80bb57ae74d8beccf49a4d6819f4c27ccb30363f
Redaction-F <koebijakobiteorhang@gmail.com>
2025-12-23 11:41:53 +0900

    fix format arg

--- src/format.ts
+++ src/format.ts
@@ -1,55 +1,58 @@
-const wideWidthChar = (value: string) : boolean => {
+const isWideChar = (value: string) : boolean => {
   const utfPoint: number = value.charCodeAt(0);
   return 12288 <= utfPoint && utfPoint <= 40959;
 }
 
-const equalWidthLength = (value: string): {value: string, length: number} => {
-  let wideCount: number = 0;
+const equalWidthLength = (value: string, widthRate?: {narrow: number, wide: number}): {value: string, length: number} => {
+  const {narrow: narrowRate, wide: wideRate} = widthRate === undefined ? {narrow: 1, wide: 2} : widthRate;
+
+  let wideCountDown: number = 0;
   let count: number = 0;
   for (const c of value) {
-    if (wideWidthChar(c)) {
-      if (wideCount === 0) {
-        wideCount = 3;
-        count += 5;
+    if (isWideChar(c)) {
+      if (wideCountDown === 0) {
+        wideCountDown = narrowRate;
+        count += wideRate;
       }
-      wideCount -= 1;
+      wideCountDown -= 1;
     } else {
       count += 1;
     }
   }
   return {
-    value: `${"　".repeat(wideCount)}${value}`,
+    value: `${"　".repeat(wideCountDown)}${value}`,
     length: count
   };
 }
 
-export const equalWidthFormat = (value: string, dight: number, zeroPadding?: boolean, cut?: boolean): string => {
+export const equalWidthFormat = (value: string, dight: number, widthRate?: {narrow: number, wide: number}, zeroPadding?: boolean, cut?: boolean): string => {
+  const {narrow: narrowRate, wide: wideRate} = widthRate === undefined ? {narrow: 1, wide: 2} : widthRate;
   const padding: string = zeroPadding === true ? "0" : " ";
-  const {value: wideFormattedValue, length} = equalWidthLength(value);
+
+  const {value: wideFormattedValue, length} = equalWidthLength(value, widthRate);
   const formattedValue = `${padding.repeat(dight > length ? dight - length : 0)}${wideFormattedValue}`;
 
   if (dight < length && cut === true) {
     let cuttedString: string = "";
-    let wideCount: number = 0;
+    let wideCountDown: number = 0;
     let count: number = 0;
     for (const c of value) {
-      if (wideWidthChar(c)) {
-        if (wideCount === 0) {
-          if (count >= dight - 8) {
-            count -= 5;
-            cuttedString = `${" ".repeat(wideCount)}${cuttedString}${".".repeat(dight - count)}`;
+      if (isWideChar(c)) {
+        if (wideCountDown === 0) {
+          if (count >= dight - (3 + wideRate)) {
+            cuttedString = `${" ".repeat(wideCountDown)}${cuttedString}${".".repeat(dight - count)}`;
             break;
           }
-          wideCount = 3;
-          count += 5;
+          wideCountDown = narrowRate;
+          count += wideRate;
         }
-        wideCount -= 1;
+        wideCountDown -= 1;
       } else {
         count += 1;
       }
       cuttedString += c;
       if (count >= dight - 3) {
-        cuttedString = `${" ".repeat(wideCount)}${cuttedString}${".".repeat(dight - count)}`;
+        cuttedString = `${" ".repeat(wideCountDown)}${cuttedString}${".".repeat(dight - count)}`;
         break;
       }
     }
